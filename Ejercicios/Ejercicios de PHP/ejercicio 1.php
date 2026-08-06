d592748c2fc72715e98d55785a6dac75bba9e7bf
Matias Suiva <matiassuiva91939@gmail.com>
2026-08-06 12:07:03 -0300

    php desde este github creo nose como anda todavia

--- Ejercicios/Ejercicios de PHP/ejercicio 1.php
+++ Ejercicios/Ejercicios de PHP/ejercicio 1.php
@@ -1,4 +1,12 @@
 <?php
 // mostrar nombre
 echo "Mi nombre es matias";
+echo "<br>";
+echo "droga";
+echo "Escribí el primer número: "; 
+$num1 = trim(fgets(STDIN)); 
+echo "Escribí el segundo número: ";
+ $num2 = trim(fgets(STDIN));
+$suma = $num1 + $num2; 
+echo "La suma es: $suma ";
 ?>
