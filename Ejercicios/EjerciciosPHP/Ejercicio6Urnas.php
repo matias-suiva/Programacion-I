64f3e867654d6cbc7c69b5402806dea2aa316d9c
Matias Suiva <matiassuiva91939@gmail.com>
2026-08-12 11:08:56 -0300

    agregamos mas opciones al menu y ahora para salir si finaliza el programa

--- Ejercicios/EjerciciosPHP/Ejercicio6Urnas.php
+++ Ejercicios/EjerciciosPHP/Ejercicio6Urnas.php
@@ -22,12 +22,24 @@
 
     $probMismoColor = ($probNegraA * $probNegraB) + ($probBlancaA * $probBlancaB); // Probabilidad de sacar bolas del mismo color de ambas urnas
     $probDistintoColor = ($probNegraA * $probBlancaB) + ($probBlancaA * $probNegraB); // Probabilidad de sacar bolas de distinto color de ambas urnas
-    
+    // probabilidad de sacar bola negra de la urna A
+    $probnegraA = $negrasA / $totalA;
+    // probabilidad de sacar bola blanca de la urna A  
+    $probBlancaA = $blancasA / $totalA;
+    // probabilidad de sacar bola negra de la urna B
+    $probnegraB = $negrasB / $totalB;
+    //probabilidad de sacar bola blanca de la urna B
+    $probBlancaB = $blancasB / $totalB;
+
     while (true) {
 
     echo "\n1. Probabilidad de mismo color\n";
     echo "2. Probabilidad de distinto color\n";
-    echo "3. Salir\n";
+    echo "3. Probabilidad de sacar bola negra de la urna A\n";
+    echo "4. Probabilidad de sacar bola blanca de la urna A\n";
+    echo "5. Probabilidad de sacar bola negra de la urna B\n";
+    echo "6. Probabilidad de sacar bola blanca de la urna B\n";
+    echo "7. Salir\n";
 
     echo "Seleccione una opción: ";
     $opcion = intval(trim(fgets(STDIN)));
@@ -44,12 +56,27 @@
         break;
 
     case 3:
-        echo "Programa finalizado.\n";
+        echo "La probabilidad de sacar bola negra de la urna A es: " . ($probnegraA * 100) . "%\n";
         break;
+
+    case 4:
+        echo "La probabilidad de sacar bola blanca de la urna A es: " . ($probBlancaA * 100) . "%\n";
+        break;
+
+    case 5:
+        echo "La probabilidad de sacar bola negra de la urna B es: " . ($probnegraB * 100) . "%\n";
+        break;
+
+    case 6:
+        echo "La probabilidad de sacar bola blanca de la urna B es: " . ($probBlancaB * 100) . "%\n";
+        break;
+
+    case 7:
+        echo "Programa finalizado.\n";
+        exit;
 
     default:
         echo "Opción inválida.\n";
         }   
     }
 ?>
-
