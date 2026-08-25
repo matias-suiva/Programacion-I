c6f9b3106721a038a0d16eb3db133e85ee5cbe8a
Matias Suiva <matiassuiva91939@gmail.com>
2026-08-25 14:17:02 -0300

    cambios para o

--- php/ejercicio2.php
+++ php/ejercicio2.php
@@ -1,16 +1,19 @@
 <?php
-// definimos dos numeros
-$num1 = 10;
-$num2 = 20;
-// mostramos los numeros
-echo "Los numeros son: $num1 y $num2\n";
-// mostramos la suma, la resta, la multiplicacion y la division de los dos numeros
-$suma = $num1 + $num2;
-$res = $num1 - $num2;
-$mul = $num1 * $num2;
-$div = $num1 / $num2;
-echo "Suma: " . $suma . "\n";
-echo "Resta: " . $res . "\n";
-echo "Multiplicación: " . $mul . "\n";
-echo "División: " . $div . "\n";
-
+    // definimos dos numeros
+    //echo "Introduce el primer numero: ";
+    $num1 = 20;
+    //echo "Introduce el segundo numero: ";
+    $num2 = 22;
+    // mostramos los numeros
+    echo "Los numeros son: $num1 y $num2";
+    echo "<br>";
+    // mostramos la suma, la resta, la multiplicacion y la division de los dos numeros
+    $suma = $num1 + $num2;
+    $res = $num1 - $num2;
+    $mul = $num1 * $num2;
+    $div = $num1 / $num2;
+    echo "Suma: " . $suma . "<br>";
+    echo "Resta: " . $res . "<br>";
+    echo "Multiplicación: " . $mul . "<br>";
+    echo "División: " . $div . "<br>";
+?>
--- php/ejercicio3.php
+++ php/ejercicio3.php
@@ -1,9 +1,14 @@
 <?php
-// guardamos un numero en una variable
-$numero = intval.(trim(fgets(STDIN)));
-// si la variable es mayor o igual a 100, mostrar "La bobina está encendida a toda potencia", sino "La bobina está en modo bajo consumo".
-if ($numero >= 100) {
-    echo "La bobina está encendida a toda potencia\n";
-} else {
-    echo "La bobina está en modo bajo consumo\n";
-}
+
+    // guardamos un numero en una variable
+    $numero = 50;
+    // si la variable es mayor o igual a 100, mostrar "La bobina está encendida a toda potencia", sino "La bobina está en modo bajo consumo".
+    if ($numero >= 100) 
+            {
+            echo "La bobina está encendida a toda potencia⚡<br>";
+        }
+    else 
+        {
+            echo "La bobina está en modo bajo consumo<br>";
+        }
+?>
